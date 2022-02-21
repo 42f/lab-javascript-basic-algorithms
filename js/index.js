469e0ad20308d98ba8897aeac27ef766c569cbf8
bvalette <bvalette@student.42.fr>
2022-02-21 21:24:36 +0100

    bonus 2 implemented

--- js/index.js
+++ js/index.js
@@ -85,3 +85,37 @@ do {
 } while (indexOfTarget !== -1)
 
 console.log(countOfTarget);
+
+
+function isPalindrome(phraseToCheck) {
+	const len = phraseToCheck.length;
+	let isPalindrom = true;
+
+	for (let i = 0, y = len - 1; i < len / 2 && y > len / 2; i++, y--) {
+
+		while (i < len / 2 && !isLetter(phraseToCheck[i])) {
+			i++;
+		}
+		while (y > len / 2 && !isLetter(phraseToCheck[y])) {
+			y--;
+		}
+		if (phraseToCheck[i].toLocaleLowerCase() !== phraseToCheck[y].toLocaleLowerCase()) {
+			isPalindrom = false;
+			break;
+		}
+	}
+	console.log(`${isPalindrom ? 'Here is a palindrome: ' : 'Not a palindrome... '}"${phraseToCheck}"`);
+}
+
+isPalindrome("A man, a plan, a canal, Panama!");
+isPalindrome("Amor, Roma");
+isPalindrome("race car");
+isPalindrome("stack cats");
+isPalindrome("step on no pets");
+isPalindrome("taco cat");
+isPalindrome("put it up");
+isPalindrome("Was it a car or a cat I saw?");
+isPalindrome("No 'x' in Nixon");
+isPalindrome("No '' in Nixon");
+isPalindrome("abc yxxcba");
+
